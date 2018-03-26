1acbaf9695dbcca351b45cd4da709f85856b54bb
hoge-jafer <zhong39@live.com>
2018-03-26 15:56:38 +0800

    修改输出内容

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitLearnDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("GitLearnDemo!");
         }
     }
 }
